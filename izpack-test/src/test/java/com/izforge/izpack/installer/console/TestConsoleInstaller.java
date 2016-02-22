76f78e129109096c4fc58f71469eed45ef1fa52a
Zdeněk Vaník <zdenekvanik@gmail.com>
2016-02-22 16:41:24 +0100

    Fix tests

--- izpack-test/src/test/java/com/izforge/izpack/installer/console/TestConsoleInstaller.java
+++ izpack-test/src/test/java/com/izforge/izpack/installer/console/TestConsoleInstaller.java
@@ -23,7 +23,6 @@ package com.izforge.izpack.installer.console;
 
 import com.izforge.izpack.installer.data.ConsoleInstallData;
 import com.izforge.izpack.installer.data.UninstallDataWriter;
-import com.izforge.izpack.installer.requirement.RequirementsChecker;
 import com.izforge.izpack.test.util.TestConsole;
 import com.izforge.izpack.test.util.TestHousekeeper;
 
@@ -42,18 +41,16 @@ public class TestConsoleInstaller extends ConsoleInstaller
      *
      * @param panels       the panels
      * @param installData  the installation date
-     * @param requirements the installation requirements
      * @param writer       the uninstallation data writer
      * @param console      the console
      * @param housekeeper  the house-keeper
      * @throws Exception for any error
      */
-    public TestConsoleInstaller(ConsolePanels panels, ConsoleInstallData installData,
-                                RequirementsChecker requirements, UninstallDataWriter writer,
+    public TestConsoleInstaller(ConsolePanels panels, ConsoleInstallData installData, UninstallDataWriter writer,
                                 TestConsole console, TestHousekeeper housekeeper)
             throws Exception
     {
-        super(panels, installData, requirements, writer, console, housekeeper);
+        super(panels, installData, writer, console, housekeeper);
     }
 
     /**
