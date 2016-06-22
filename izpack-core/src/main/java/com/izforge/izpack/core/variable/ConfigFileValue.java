d2071fea3d33f724fb74fa5a688ac27252a315dc
Tom Helpstone <DTH@freenet.de>
2016-06-22 09:45:56 +0200

    make code more readable

--- izpack-core/src/main/java/com/izforge/izpack/core/variable/ConfigFileValue.java
+++ izpack-core/src/main/java/com/izforge/izpack/core/variable/ConfigFileValue.java
@@ -24,7 +24,6 @@ package com.izforge.izpack.core.variable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
-import java.util.HashSet;
 import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -217,12 +216,11 @@ public abstract class ConfigFileValue extends ValueImpl implements Serializable
     @Override
     public Set<String> getUnresolvedVariableNames()
     {
-        Set<String> unresolvedNames = new HashSet<String>();
-        unresolvedNames.add(key);
+        Set<String> unresolvedNames = parseUnresolvedVariableNames(key); 
         if (type == CONFIGFILE_TYPE_INI)
         {
-            unresolvedNames.add(section);
+            unresolvedNames.addAll(parseUnresolvedVariableNames(section));
         }
-        return parseUnresolvedVariableNames(unresolvedNames.toArray(new String[unresolvedNames.size()]));
+        return unresolvedNames;
     }
 }
