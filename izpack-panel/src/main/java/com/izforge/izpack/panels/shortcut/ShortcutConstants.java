8c464fd048b821cf08e9f551d05c1dbb12f03b8a
auswells <32709593+auswells@users.noreply.github.com>
2018-02-26 11:07:47 -0600

    Revert "Added the ability to disable the previous navigation button on the sh…"

--- izpack-panel/src/main/java/com/izforge/izpack/panels/shortcut/ShortcutConstants.java
+++ izpack-panel/src/main/java/com/izforge/izpack/panels/shortcut/ShortcutConstants.java
@@ -50,8 +50,6 @@ public class ShortcutConstants
 
     static final String SPEC_KEY_PACKS = "createForPack";
 
-    static final String SPEC_KEY_PREVIOUS_DISABLED = "previousDisabled";
-
     // ------------------------------------------------------
     // spec file key attributes
     // ------------------------------------------------------
