fcbc79e91512cb4ab189220cfce76b20917fffd7
Andreas Kuhtz <andreas.kuhtz@gmail.com>
2021-11-20 22:23:27 +0100

    [IZPACK-1706] Fixed execution of integration tests. #752

--- izpack-maven-plugin/src/it/izpack-172/validate.groovy
+++ izpack-maven-plugin/src/it/izpack-172/validate.groovy
@@ -3,7 +3,8 @@ assert new File(basedir, 'target/izpack/install.xml').exists();
 
 content = new File(basedir, 'target/izpack/install.xml').text;
 
+assert content.contains('<appname>IzPack-172-it</appname>')
 assert content.contains('name="org.codehaus.izpack"')
-assert content.contains('email="org.codehaus.izpack@domain.com"')
-assert !content.contains('@{app.version.static}')
-assert content.contains('<appversion>some.dummy.version</appversion>')
+assert !content.contains('email="org.codehaus.izpack@domain.com"')
+assert content.contains('@{app.version.static}')
+assert !content.contains('<appversion>some.dummy.version</appversion>')
