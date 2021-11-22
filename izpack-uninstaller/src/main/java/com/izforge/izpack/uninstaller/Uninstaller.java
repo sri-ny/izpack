e4a1a6e476209c9bcfe2a032cbf9fd268078295c
Rémi Alvergnat <toilal.dev@gmail.com>
2021-11-22 17:02:07 +0100

    [IZPACK-1707] Add long arguments support to Uninstaller (-console/-force) (#733)

--- izpack-uninstaller/src/main/java/com/izforge/izpack/uninstaller/Uninstaller.java
+++ izpack-uninstaller/src/main/java/com/izforge/izpack/uninstaller/Uninstaller.java
@@ -82,7 +82,7 @@ public class Uninstaller
         boolean console = false;
         for (String arg : args)
         {
-            if (arg.equals("-c"))
+            if (arg.equals("-c") || arg.equals("-console"))
             {
                 console = true;
             }
@@ -129,7 +129,7 @@ public class Uninstaller
             boolean force = false;
             for (String arg : args)
             {
-                if (arg.equals("-f"))
+                if (arg.equals("-f") || arg.equals("-force"))
                 {
                     force = true;
                 }
@@ -156,11 +156,11 @@ public class Uninstaller
 
                     for (String arg : args)
                     {
-                        if (arg.equals("-f"))
+                        if (arg.equals("-f") || arg.equals("-force"))
                         {
                             forceOptionState = true;
                         }
-                        else if (arg.equals("-x"))
+                        else if (arg.equals("-x") || arg.equals("-hide-force-option"))
                         {
                             displayForceOption = false;
                         }
