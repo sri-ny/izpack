953ce5c273cec9adc34e62477ab17a815ac413f4
Hitesh A. Bosamiya <45116032+hiteshbosamiya@users.noreply.github.com>
2024-02-22 00:56:25 +0530

    [IZPACK-1759] Update synLangPacks.groovy script to preserve the format (#868)

--- izpack-core/src/main/resources/com/izforge/izpack/bin/langpacks/installer/syncLangPacks.groovy
+++ izpack-core/src/main/resources/com/izforge/izpack/bin/langpacks/installer/syncLangPacks.groovy
@@ -1,5 +1,4 @@
-import groovy.xml.DOMBuilder
-import groovy.xml.dom.DOMCategory
+package com.izforge.izpack.bin.langpacks.installer
 
 def languages = ['bgr', 'bra', 'cat', 'ces', 'chn', 'dan', 'deu', 'ell', 'eus', 'fa',
                  'fin', 'fra', 'glg', 'hun', 'idn', 'ita', 'jpn', 'kor', 'msa', 'nld',
@@ -9,46 +8,49 @@ languages.each {syncLangPack(it)}
 
 private void syncLangPack(String lang) {
     println "Synchronizing langpack for $lang..."
-    def langpack = getEnglishLangPack()
-    use(DOMCategory) {
-        def strings = langpack.str
-        def refStrings = getReferenceStrings(lang)
-        for (def str in strings) {
-            def id = str.attributes['id']
-            def txt = findRefString(refStrings, id)
-            if (txt) {
-                str['@txt'] = txt
-            }
+    def refStrings = getReferenceStrings(lang)
+    def writer = new File(lang + '.xml').newWriter("UTF-8")
+    new File('eng.xml').eachLine("UTF-8", { line ->
+        writer.writeLine(getLangString(line, refStrings))
+    })
+    writer.flush()
+    writer.close()
+}
+
+private static String getLangString(String line, def refStrings) {
+    int startIndex = line.indexOf('    <str id="')
+    if (startIndex == -1) {
+        return line
+    }
+    int endIndex = line.indexOf('" txt="')
+    if (endIndex == -1) {
+        return line
+    }
+    String id = line.substring(startIndex + 13, endIndex)
+    String txt = refStrings.get(id)
+    if (txt != null) {
+        return '    <str id="' + id + '" txt="' + txt + '"/>'
+    }
+    return line
+}
+
+private static def getReferenceStrings(String lang) {
+    def map = [:]
+    new File(lang + '.xml').eachLine("UTF-8", { line ->
+        int startIndex = line.indexOf('    <str id="')
+        if (startIndex == -1) {
+            return
         }
-    }
-    new File(lang + '.xml').write(langpack as String, 'UTF-8')
-}
-
-private def getReferenceStrings(String lang) {
-    def input = new File(lang + '.xml').getText('UTF-8')
-    def doc = DOMBuilder.newInstance().parseText(input)
-    def encoding = doc.getXmlEncoding()
-    if (!encoding.equalsIgnoreCase('UTF-8')) {
-        println "Encoding is $encoding for $lang"
-        input =  new File(lang + '.xml').getText(encoding)
-        doc = DOMBuilder.newInstance().parseText(input)
-    }
-    def langpack = doc.documentElement
-    use(DOMCategory) {
-        return langpack.str
-    }
-}
-
-private def getEnglishLangPack() {
-    def input = new File('eng.xml').getText("UTF-8")
-    def doc = DOMBuilder.newInstance().parseText(input)
-    return doc.documentElement
-}
-
-private String findRefString(def refStrings, String id) {
-    def node = refStrings.find({it.attributes['id'] == id})
-    if (node) {
-        return node.attributes['txt']
-    }
-    return null
+        int endIndex = line.indexOf('" txt="')
+        if (endIndex == -1) {
+            return
+        }
+        String id = line.substring(startIndex + 13, endIndex)
+        startIndex = endIndex + 7
+        endIndex = line.length() - 3
+        String txt = line.substring(startIndex, endIndex)
+        map.put(id, txt)
+        return
+    })
+    return map
 }
